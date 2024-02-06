0fc1574c33fc4fe51174a0402e77156dd6a04544
Balgyn <baiborinova03@gmail.com>
2024-02-06 20:59:28 +0600

    омагад №3

--- ready-html/js/mainpage.js
+++ ready-html/js/mainpage.js
@@ -1,8 +1,30 @@
-jQuery(".block_width").draggable({
-    cursor: "move",
-    axis: "x",
-    stop: function() {
-        if(jQuery(this).position().left > 1 || jQuery(this).position().left < -800)
-            jQuery(this).css("left", "10px");
-    }
+const scrollableDiv = document.getElementById('container');
+let isMouseDown = false;
+let startX;
+let scrollLeft;
+
+// Начинаем скроллирование при зажатой левой кнопке мыши
+scrollableDiv.addEventListener('mousedown', (e) => {
+    isMouseDown = true;
+    startX = e.pageX - scrollableDiv.offsetLeft;
+    scrollLeft = scrollableDiv.scrollLeft;
 });
+
+// Продолжаем скроллирование при перемещении мыши, если кнопка зажата
+scrollableDiv.addEventListener('mousemove', (e) => {
+    if (!isMouseDown) return;
+    e.preventDefault();
+    const x = e.pageX - scrollableDiv.offsetLeft;
+    const walk = (x - startX) * 2; // Умножаем на 2 для более быстрого скроллинга
+    scrollableDiv.scrollLeft = scrollLeft - walk;
+});
+
+// Заканчиваем скроллирование при отпускании левой кнопки мыши
+scrollableDiv.addEventListener('mouseup', () => {
+    isMouseDown = false;
+});
+
+// Отменяем скроллирование при покидании курсором области скроллинга
+scrollableDiv.addEventListener('mouseleave', () => {
+    isMouseDown = false;
+});tListener('keydown', handleArrowKeys);
